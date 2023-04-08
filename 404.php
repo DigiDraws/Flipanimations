c42fd36965c50ea99671c49eafe6063d1a8ada12
enzo2323 <58128131+enzo2323@users.noreply.github.com>
2023-04-08 06:48:29 -0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: sans-serif;
+    }
+        body{
+            background: #1a84d3;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .container-404{
+            text-align: center;
+            width: 100%;
+            max-width: 400px;
+            background: #fff;
+            padding: 20px;
+            box-shadow: 0px 1px 10px rgba(0,0,0,0.3);
+            border-radius: 5px;
+            line-height: 1.7;
+            color: #434343;
+        }
+        .boton{
+            display: inline-block;
+            margin-top: 15px;
+            text-decoration: none;
+            background: #1a84d3;
+            color: #fff;
+            padding: 10px 15px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-404">
+        <h1>La página que ha solicitado no se encuentra disponible</h1>
+        <a href="/index.html" class="boton">Volver atras</a>
+    </div>
+</body>
+
+</html>
